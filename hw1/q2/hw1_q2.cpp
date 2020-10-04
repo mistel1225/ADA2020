e19ac020735009c78fd5f513efe0666954e2e85c
mistel1225 <ha19971225@gmail.com>
2020-10-04 14:26:21 +0800

    todo

---
+++ hw1/q2/hw1_q2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include "helper.h"
+using namespace std;
+using namespace Memory;
+
+int main(){
+	int N, M;
+	cin>>N>>M;
+	vector<vector<long long>> damage_record(M, vector<long long>(2));
+	string in;
+	long long c, l, r, d, p, k;
+	if(M<=N){
+		for(int i=0; i<N; i++){
+			cin>>in;
+			if(in=="p"){
+				cin>>c>>d;
+			}
+				
+		}
+	}
+	else{
+		
+	}
+}
---
+++ hw1/q2/string_sample.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+	string s;
+	for(int i=0; i<3; i++){
+		cin>>s;
+		cout<<s<<endl;
+	}
+}
