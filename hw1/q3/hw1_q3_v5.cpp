24dcb39bb04fb4e94a5a6d16f15e98abeaa72246
mistel1225 <ha19971225@gmail.com>
2020-10-19 17:14:35 +0800

    todo

--- hw1/q3/hw1_q3_v5.cpp
+++ hw1/q3/hw1_q3_v5.cpp
@@ -38,14 +38,16 @@ long long ADA_party_rec(int first, int last, int K, long long *candies, long lon
 		else
 			max[i]=max[i-1];
 	}
-	for(int i=0; i<K; i++)
-		mod_array[i]= 0;	
+	/*for(int i=0; i<K; i++)
+		mod_array[i]= 0;*/
+	vector<int> index_record;
 	//case1: both max and min are in the left
 	l=mid, r=mid+1;
 	while(l>=first){
 		while(r<=last){
 			if(max[l]>max[r] && min[l]<min[r]){
 				mod_array[prefix_sum[r+1]%K]++;
+				index_record.push_back(r);
 				r++;
 			}
 			else
@@ -54,6 +56,9 @@ long long ADA_party_rec(int first, int last, int K, long long *candies, long lon
 		sumM+=mod_array[(prefix_sum[l]+max[l]+min[l])%K];
 		l--;
 	}
+	for(int i=0; i<index_record.size(); i++)
+		mod_array[prefix_sum[index_record[i]+1]%K]--;
+	index_record.clear();
 	//case2: both max and min are in the right
 	l=mid, r=mid+1;
 	for(int i=0; i<K; i++)
@@ -62,6 +67,7 @@ long long ADA_party_rec(int first, int last, int K, long long *candies, long lon
 		while(l>=first){
 			if(max[r]>max[l] && min[r]<min[l]){
 				mod_array[prefix_sum[l]%K]++;
+				index_record.push_back(l);
 				l--;
 			}
 			else
@@ -70,31 +76,43 @@ long long ADA_party_rec(int first, int last, int K, long long *candies, long lon
 		sumM+=mod_array[(prefix_sum[r+1]-max[r]-min[r])%K];
 		r++;
 	}
+	for(int i=0; i<index_record.size(); i++)
+		mod_array[prefix_sum[index_record[i]]%K]--;
 	
+	vector<int> index_record_plus, index_record_minus;
 	//case3: max in the left, min in the right
 	l=mid, r=mid+1;
 	int p=r, q=r;
-	for(int i=0; i<K; i++)
-		mod_array[i] = 0;
+	/*for(int i=0; i<K; i++)
+		mod_array[i] = 0;*/
 	while(l>=first){
 			while(q<=last){
 				if(max[q]>max[l])
 					break;
-				else if(q>=p)
+				else if(q>=p){
 					mod_array[(prefix_sum[q+1]-min[q])%K]+=1;
+					index_record_plus.push_back(q);
+				}
 				q++;
 			}
 			while(p<=last){
 				if(min[p]<min[l])
 					break;
-				else if(p<q)
+				else if(p<q){
 					mod_array[(prefix_sum[p+1]-min[p])%K]-=1;
+					index_record_minus.push_back(p);
+				}
 				p++;
 			}
 		sumM+=mod_array[(prefix_sum[l]+max[l])%K];
 		l--;
 	}
-	
+	for(int i=0; i<index_record_plus.size(); i++)
+		mod_array[(prefix_sum[index_record_plus[i]+1]-min[index_record_plus[i]])%K]--;
+	for(int i=0; i<index_record_minus.size(); i++)
+		mod_array[(prefix_sum[index_record_minus[i]+1]-min[index_record_minus[i]])%K]++;
+	index_record_plus.clear();
+	index_record_minus.clear();
 	//case4: max in the right, min in the left
 	l=mid, r=mid+1;
 	p=r, q=r;
