50256907dcbdc4e1e28510e8f5f6fcbe8a798d9c
mistel1225 <ha19971225@gmail.com>
2020-10-21 08:15:20 +0800

    123

--- hw2/q1/hw2_q1.cpp
+++ hw2/q1/hw2_q1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 
@@ -6,24 +7,34 @@ int main(){
 	long long N, K, A, B;
 	cin>>N>>K>>A>>B;
 	long long grid[N];
-	//calculate bonus in each grid, -1 means grid[i] is build a tower
+	//calculate bonus in each grid, -1 means grid[i] is build with a tower
 	long long bonus[N]={0};
 	for(long long i=0; i<N; i++)
 		cin>>grid[i];
 	
 	long long total_defense=0;
-	long long temp_max_defense;
+	long long temp_max_loc;
 	for(long long i=0; i<K; i++){
-		for(long long k=0; k<N; k++)
-			if(grid[k]!=-1)
-				temp_max_defense = k;
-		for(long long j=0; j<N; j++){
+		for(long long j=0; j<N; j++)
+			if(bonus[j]!=-1)
+				temp_max_loc = j;
+		for(long long j=temp_max_loc+1; j<N; j++)
+			if(bonus[j]!=-1)
+				if(grid[j]+bonus[j]>grid[temp_max_loc]+bonus[temp_max_loc])
+					temp_max_loc=j;
+		//grid[i] is build with a tower
+		bonus[temp_max_loc]=-1;
+		for(long long j=temp_max_loc-B; j<=temp_max_loc+B&&j<N; j++){
 			if(bonus[j]!=-1){
-				if(grid[j]+bonus[j]>grid[temp_max_defense]+bonus[temp_max_defense]){
-					temp_max_defense=j;
-				}
+				if(B-abs(temp_max_loc-j)>0)
+					bonus[j]+=A*(B-abs(temp_max_loc-j));
 			}
 		}
+		if(total_defense+grid[temp_max_loc]+bonus[temp_max_loc]>total_defense)
+			total_defense+=grid[temp_max_loc]+bonus[temp_max_loc];
+		else
+			break;
 	}
-	
+	cout<<total_defense<<endl;
+	return 0;
 }
