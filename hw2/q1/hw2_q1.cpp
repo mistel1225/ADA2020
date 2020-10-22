4ee7f3adf20295ba1b334d23b961f4c7df7c6869
mistel1225 <ha19971225@gmail.com>
2020-10-22 21:20:55 +0800

    todo

--- hw2/q1/hw2_q1.cpp
+++ hw2/q1/hw2_q1.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
-
+#define DEBUG
 
 int main(){
 	long long N, K, A, B;
@@ -11,29 +11,51 @@ int main(){
 	long long bonus[N]={0};
 	for(long long i=0; i<N; i++)
 		cin>>grid[i];
-	
+	#ifdef DEBUG
+	for(int i=0; i<N; i++)
+		cout<<grid[i]<<" ";
+	cout<<endl;
+	for(int i=0; i<N; i++)
+		cout<<bonus[i]<<" ";
+	cout<<endl;
+	#endif
 	long long total_defense=0;
 	long long temp_max_loc;
-	for(long long i=0; i<K; i++){
-		for(long long j=0; j<N; j++)
+	long long i, j;
+	for(i=0; i<K; i++){
+		for(j=0; j<N; j++)
 			if(bonus[j]!=-1)
 				temp_max_loc = j;
-		for(long long j=temp_max_loc+1; j<N; j++)
+		for(j=temp_max_loc+1; j<N; j++)
 			if(bonus[j]!=-1)
 				if(grid[j]+bonus[j]>grid[temp_max_loc]+bonus[temp_max_loc])
 					temp_max_loc=j;
-		//grid[i] is build with a tower
-		bonus[temp_max_loc]=-1;
-		for(long long j=temp_max_loc-B; j<=temp_max_loc+B&&j<N; j++){
-			if(bonus[j]!=-1){
-				if(B-abs(temp_max_loc-j)>0)
-					bonus[j]+=A*(B-abs(temp_max_loc-j));
-			}
-		}
-		if(total_defense+grid[temp_max_loc]+bonus[temp_max_loc]>total_defense)
+		for(j=temp_max_loc+1; j<=temp_max_loc+B&&j<N; j++)
+			if(bonus[j]!=-1)
+					bonus[j]=A*(B-(j-temp_max_loc));
+			else if(bonus[j]==-1)
+				break;
+		if(bonus[j]==-1&&j<N)
+			for(long long k=j-1; k>temp_max_loc; k--)
+					bonus[k]+=A*(B-(j-k));
+		
+		for(j=temp_max_loc-1; j>=temp_max_loc-B&&j>=0; j--)
+            if(bonus[j]!=-1)
+                    bonus[j]=A*(B-(temp_max_loc-j));
+            else if(bonus[j]==-1)
+                break;
+		if(bonus[j]==-1&&j>=0)
+			for(long long k=j+1; k<temp_max_loc; j++)
+					bonus[k]+=A*(B-(k-j));
+		if(total_defense+grid[temp_max_loc]+bonus[temp_max_loc]>total_defense){
 			total_defense+=grid[temp_max_loc]+bonus[temp_max_loc];
+			bonus[temp_max_loc]=-1;
+		}
 		else
 			break;
+		#ifdef DEBUG
+		cout<<grid[temp_max_loc]<<endl;
+		#endif
 	}
 	cout<<total_defense<<endl;
 	return 0;
