79938494d1ca7f473e62b4f5a38372766df0aa1a
mistel1225 <ha19971225@gmail.com>
2020-10-23 17:05:51 +0800

    todo hw2_q1

--- hw2/q1/hw2_q1.cpp
+++ hw2/q1/hw2_q1.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <cmath>
+#include <vector>
 using namespace std;
 //#define DEBUG
 
@@ -14,6 +14,7 @@ int main(){
 	long long total_defense=0;
 	long long temp_max_loc;
 	long long i=0, j=0;
+	vector<long long> grid_index;
 	for(i=0; i<K; i++){
 		for(j=0; j<N; j++){
 			if(bonus[j]!=-1){
@@ -29,27 +30,53 @@ int main(){
 		#ifdef DEBUG
 		cout<<grid[temp_max_loc]<<endl;
 		#endif
-		for(j=temp_max_loc+1; j<=temp_max_loc+B&&j<N; j++)
-			if(bonus[j]!=-1)
+		for(j=temp_max_loc+1; j<N; j++){
+			if(bonus[j]!=-1){
+				if(B-(j-temp_max_loc)>0)	
 					bonus[j]=A*(B-(j-temp_max_loc));
+				else
+					bonus[j]=0;
+			}
 			else if(bonus[j]==-1)
 				break;
+		}
 		if(bonus[j]==-1&&j<N)
 			for(long long k=j-1; k>temp_max_loc; k--)
+				if(B-(j-k)>0)
 					bonus[k]+=A*(B-(j-k));
+				else
+					break;
 		
-		for(j=temp_max_loc-1; j>=temp_max_loc-B&&j>=0; j--)
-            if(bonus[j]!=-1)
+		for(j=temp_max_loc-1; j>=0; j--){
+            if(bonus[j]!=-1){
+				if(B-(temp_max_loc-j)>0)
                     bonus[j]=A*(B-(temp_max_loc-j));
+				else
+					bonus[j]=0;
+			}
             else if(bonus[j]==-1)
                 break;
+		}
 		if(bonus[j]==-1&&j>=0)
 			for(long long k=j+1; k<temp_max_loc; k++)
+				if(B-(k-j)>0)
 					bonus[k]+=A*(B-(k-j));
-		if(total_defense+grid[temp_max_loc]+bonus[temp_max_loc]>total_defense){
-			total_defense+=grid[temp_max_loc]+bonus[temp_max_loc];
-			bonus[temp_max_loc]=-1;
+				else
+					break;
+		bonus[temp_max_loc]=-1;
+		long long total_defense_temp=0;
+		for(long long k=0; k<N; k++){
+			if(bonus[k]==-1){
+				total_defense_temp+=grid[k];
+				grid_index.push_back(k);
+			}
 		}
+		for(long long k=0; k<grid_index.size()-1; k++)
+			if(B-(grid_index[k+1]-grid_index[k])>0)
+				total_defense_temp+=A*(B-(grid_index[k+1]-grid_index[k]));
+		grid_index.clear();
+		if(total_defense_temp>=total_defense)
+			total_defense=total_defense_temp;
 		else
 			break;
 		#ifdef DEBUG
---
+++ hw2/q1/hw2_q1_v2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+
+int main(){
+	long long N, K, A, B;
+	cin>>N>>K>>A>>B;
+	long long dp[N][K+1];
+	for(long long i=0; i<N; i++)
+		cin>>dp[i][0];
+	
+	
+}
