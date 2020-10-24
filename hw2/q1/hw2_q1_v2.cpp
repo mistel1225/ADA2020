5f55782b935898d22198e5ae6b61476a466c01f9
mistel5277 <ha19971225@gmail.com>
2020-10-24 12:31:45 +0800

    123

--- hw2/q1/hw2_q1_v2.cpp
+++ hw2/q1/hw2_q1_v2.cpp
@@ -17,16 +17,17 @@ int main(){
 		long long temp_max_loc=0;
 		long long temp_max=0;
 		for(long long j=1; j<N; j++){
+			if(dp[k-1][j-1]+A*(B-(j-(j-1)))>dp[k-1][temp_max_loc]+A*(B-(j-temp_max_loc))&&dp[k-1][j-1]+A*(B-(j-(j-1)))>dp[k-1][temp_max_loc])
+                temp_max_loc = j-1;			
 			if(dp[k-1][temp_max_loc]+A*(B-(j-temp_max_loc))>dp[k-1][temp_max_loc])
 				temp_max = dp[k-1][temp_max_loc]+A*(B-(j-temp_max_loc));
 			else
 				temp_max = dp[k-1][temp_max_loc];
-            if(dp[k-1][j-1]+A*(B-(j-(j-1)+1))>dp[k-1][temp_max_loc]+A*(B-(j-temp_max_loc+1))
-                        &&dp[k-1][j-1]+A*(B-(j-(j-1)+1))>dp[k-1][temp_max_loc])
+            /*if(dp[k-1][j-1]+A*(B-(j-(j-1)+1))>dp[k-1][temp_max_loc]+A*(B-(j-temp_max_loc+1))&&dp[k-1][j-1]+A*(B-(j-(j-1)+1))>dp[k-1][temp_max_loc])
                 temp_max_loc = j-1;
 			if(dp[k-1][j-1]+A*(B-(j-(j-1)))>temp_max)
-				temp_max = dp[k-1][j-1]+A*(B-(j-(j-1)));
-			dp[k][j] = temp_max+grid[j];
+				temp_max = dp[k-1][j-1]+A*(B-(j-(j-1)));*/
+			dp[k][j] = temp_max;
 			#ifdef DEBUG
 			cout<<dp[k][j]<<" ";
 			#endif
@@ -35,7 +36,7 @@ int main(){
 		cout<<endl;
 		#endif
 	}
-	long long max=dp[K-1][0];
+	long long max=dp[K-1][0]+grid[0];
 	/*for(long long i=0; i<K; i++)
 		for(long long j=0; j<N; j++)
 			if(dp[i][j]>max)
