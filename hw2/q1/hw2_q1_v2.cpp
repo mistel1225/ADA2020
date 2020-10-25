c48b92b75f4e6ef2671a2f42eaa7fd9322381471
mistel5277 <ha19971225@gmail.com>
2020-10-25 13:37:11 +0800

    todo fix q1 bug

--- hw2/q1/hw2_q1_v2.cpp
+++ hw2/q1/hw2_q1_v2.cpp
@@ -5,45 +5,46 @@ using namespace std;
 int main(){
 	long long N, K, A, B;
 	cin>>N>>K>>A>>B;
-	long long dp[K][N];
+	long long dp_prev[N];
+	long long dp_curr[N];
 	long long grid[N];
+	long long max;
 	for(long long i=0; i<N; i++){
 		cin>>grid[i];
-		dp[0][i]=grid[i];
+		dp_prev[i]=grid[i];
 	}
-	for(long long i=1; i<K; i++)
-		dp[i][0] = dp[i-1][0];
-	for(long long k=1; k<K; k++){
-		long long temp_max_loc=0;
-		long long temp_max=0;
-		for(long long j=1; j<N; j++){
-			/*if(dp[k-1][j-1]+A*(B-(j-(j-1)))>dp[k-1][temp_max_loc]+A*(B-(j-temp_max_loc))&&dp[k-1][j-1]+A*(B-(j-(j-1)))>dp[k-1][temp_max_loc])
-                temp_max_loc = j-1;			*/
-			if(dp[k-1][temp_max_loc]+A*(B-(j-temp_max_loc))>dp[k-1][temp_max_loc])
-				temp_max = dp[k-1][temp_max_loc]+A*(B-(j-temp_max_loc));
-			else
-				temp_max = dp[k-1][temp_max_loc];
-            if(dp[k-1][j]+A*(B-(j-j+1))>dp[k-1][temp_max_loc]+A*(B-(j-temp_max_loc+1))&&dp[k-1][j]+A*(B-(j-j+1))>dp[k-1][temp_max_loc])
-                temp_max_loc = j;
-			/*if(dp[k-1][j-1]+A*(B-(j-(j-1)))>temp_max)
-				temp_max = dp[k-1][j-1]+A*(B-(j-(j-1)));*/
-			dp[k][j] = temp_max+grid[j];
-			#ifdef DEBUG
-			cout<<dp[k][j]<<" ";
-			#endif
-		}
-		#ifdef DEBUG
-		cout<<endl;
-		#endif
-	}
-	long long max=dp[K-1][0];
-	/*for(long long i=0; i<K; i++)
-		for(long long j=0; j<N; j++)
-			if(dp[i][j]>max)
-				max = dp[i][j];*/
+	max=dp_prev[0];
 	for(long long i=0; i<N; i++)
-		if(dp[K-1][i]>max)
-			max = dp[K-1][i];
+		if(dp_prev[i]>max)
+			max = dp_prev[i];
+	for(long long k=1; k<K; k++){
+		long long max_nonebonus_loc=0;
+		long long max_bonus_loc=0;
+		long long temp_max=0;
+		dp_curr[0]=dp_prev[0];
+		for(long long j=1; j<N; j++){
+			if(dp_prev[j-1]>dp_prev[max_nonebonus_loc])
+				max_nonebonus_loc=j-1;
+			if(dp_prev[j-1]+A*(B-1)>=dp_prev[max_bonus_loc]+A*(B-(j-max_bonus_loc)))
+				max_bonus_loc = j-1;
+			if(dp_prev[max_bonus_loc]+A*(B-(j-max_bonus_loc))>dp_prev[max_nonebonus_loc])
+				temp_max = dp_prev[max_bonus_loc]+A*(B-(j-max_bonus_loc));
+			else
+				temp_max = dp_prev[max_nonebonus_loc];
+			dp_curr[j] = temp_max+grid[j];
+		}
+		for(long long j=0; j<N; j++){
+			if(dp_curr[j]>max)
+				max = dp_curr[j];
+			dp_prev[j] = dp_curr[j];
+		}
+        #ifdef DEBUG
+        for(long long i=0; i<N; i++)
+            cout<<dp_prev[i]<<" ";
+        cout<<endl;
+        #endif
+	}
+
 	cout<<max<<endl;
 	return 0;
 }
