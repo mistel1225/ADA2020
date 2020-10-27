79a1e8f7c91ea3a5925b13b5a888cc312a209ade
mistel1225 <ha19971225@gmail.com>
2020-10-27 17:10:37 +0800

    todo hw2 q2

--- hw2/q3/hw2_q2.cpp
+++ hw2/q3/hw2_q2.cpp
@@ -1,19 +1,123 @@
 #include <iostream>
 #include <vector>
-
+#include <cmath>
+using namespace std;
 int main(){
 	long long n;
+	bool flag;
 	cin>>n;
-	vector<long long> odd_vector, even_vector;
+	vector<long long> book_vector, book_vector1;
+	vector<long long> beauty_arrange;
+	book_vector.push_back(0);
+	book_vector1.push_back(0);
 	for(long long i=1; i<=n; i++){
 		long long temp;
 		cin>>temp;
-		if(i%2==0)
-			for(long long j=0; j<temp; j++)
-				even_vector.push_back(i)
-		else if(i%2==1)
-			for(long long j=0; j<temp; j++)
-				odd_vector.push_back(i);
+		book_vector.push_back(temp);
+		book_vector1.push_back(temp);
 	}
+	cin>>flag;
+	long long a, b;
+	for(long long i=1; i<=n; i++){
+		if(book_vector[i]!=0){
+			a=i;
+			break;
+		}
+	}
+	b=a+1;
+	long long insert_num=0;
+	while(a!=n&&b!=n){
+		if(a<b){
+			if(book_vector[b]==0){
+				while(book_vector[a]!=0){
+					if(abs(b-beauty_arrange.back())==1){
+						beauty_arrange.push_back(b);
+						beauty_arrange.push_back(a);
+						insert_num+=1;
+						book_vector[a]-=1;
+					}
+					else if(abs(a-beauty_arrange.back())==1){
+						beauty_arrange.push_back(a);
+						beauty_arrange.push_back(b);
+						insert_num+=1;
+						book_vector[a]-=1;
+					}
+				}
+				if(abs(b-beauty_arrange.back())==1){
+					beauty_arrange.push_back(b);
+					insert_num+=1;
+				}
+				a=a+2, b=b+2;
+				continue;
+			}
+			if(book_vector[a]==0){
+				a=a+2;
+				continue;
+			}
+			if(abs(a-beauty_arrange.back())==1){
+				beauty_arrange.push_back(a);
+				beauty_arrange.push_back(b);
+			}
+			else if(abs(b-beauty_arrange.back())==1){
+				beauty_arrange.push_back(b);
+				beauty_arrange.push_back(a);
+			}
+			book_vector[a]-=1;
+			book_vector[b]-=1;
+		}
+		else if(a>b){
+			if(book_vector[a]==0){
+				while(book_vector[b]!=0){
+					if(abs(a-beauty_arrange.back())==1){
+						beauty_arrange.push_back(a);
+						beauty_arrange.push_back(b);
+						insert_num+=1;
+						book_vector[b]-=1;
+					}
+					else if(abs(b-beauty_arrange.back())==1){
+						beauty_arrange.push_back(b);
+						beauty_arrange.push_back(a);
+						insert_num+=1;
+						book_vector[b]-=1;
+					}
+				}
+				if(abs(a-beauty_arrange.back())==1){
+					beauty_arrange.push_back(a);
+					insert_num+=1;
+				}
+				a=a+2, b=b+2;
+				continue;
+			}
+			if(book_vector[b]==0){
+				b=b+2;
+				continue;
+			}
+			if(abs(a-beauty_arrange.back())==1){
+				beauty_arrange.push_back(a);
+				beauty_arrange.push_back(b);
+			}
+			else if(abs(b-beauty_arrange.back())==1){
+				beauty_arrange.push_back(b);
+				beauty_arrange.push_back(a);
+			}
+			book_vector[a]-=1;
+			book_vector[b]-=1;
+		}
+	}
+	if(a==n){
+		b=a-1;
+		
 	
+	
+	for(long long i=1; i<=n; i++){
+		if(book_vector[i]!=0){
+			a=i;
+			break;
+		}
+	}
+	b=a+1;
+	long long insert_num2 =0;
+	while(a!=n&&b!=n){
+		
+	}
 }
