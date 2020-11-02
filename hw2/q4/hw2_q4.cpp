722f703537ed9fb1feef2fbf7fd8df0f896bf507
mistel5277 <ha19971225@gmail.com>
2020-11-03 00:14:38 +0800

    todo hw2_q4

--- hw2/q4/hw2_q4.cpp
+++ hw2/q4/hw2_q4.cpp
@@ -4,19 +4,49 @@
 #include <algorithm>
 using namespace std;
 #define DEBUG
+
+class segment{
+	public:
+		long long id;
+		long long L;
+		long long R;
+		long long W;
+};
+
+bool sortbyL(const segment &s1, const segment &s2){
+	return s1.L<s2.L;
+}
+bool operator<(const segment &s1, const segment &s2){
+	return s1.R < s2.R;
+}
+bool operator>(const segment &s1, const segment &s2){
+	return s1.R > s2.R;
+}
 int main(){
 	long long N, M, K;
 	cin>>N>>M>>K;
-	vector<vector<long long>> segments(N, vector<long long>(3));
-	long long w_sum=0;
+	vector<segment> segment_set;
 	for(long long i=0; i<N; i++){
-		cin>>segments[i][0]>>segments[i][1]>>segments[i][2];
-		w_sum+=segments[i][2];
+		segment seg_in;
+		seg_in.id=i;
+		cin>>seg_in.L>>seg_in.R>>seg_in.W;
+		segment_set.push_back(seg_in);
 	}
 	//sort by L_i
-	sort(segments.begin(), segments.end());
-	#ifdef DEBUG
+	sort(segment_set.begin(), segment_set.end(), sortbyL);
+	#ifdef DEBUG0
 	for(long long i=0; i<N; i++)
-		cout<<segments[i][0]<<" "<<segments[i][1]<<" "<<segments[i][2]<<endl;
+		cout<<segment_set[i].id<<" "<<segment_set[i].L<<" "<<segment_set[i].R<<" "<<segment_set[i].W<<endl;
+	#endif
+	
+	//init heap
+	priority_queue<segment, vector<segment>, less<vector<segment>::value_type>> seg_heap;
+	#ifdef DEBUG0
+	for(long long i=0; i<N; i++)
+		seg_heap.push(segment_set[i]);
+	while(!seg_heap.empty()){
+		cout<<seg_heap.top().id<<" "<<seg_heap.top().L<<" "<<seg_heap.top().R<<" "<<seg_heap.top().W<<endl;
+		seg_heap.pop();
+	}
 	#endif
 }
