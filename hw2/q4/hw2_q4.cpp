ef920e127ee2f6a30dc22c926e2c8f2081b3c1b8
mistel5277 <ha19971225@gmail.com>
2020-11-03 23:22:35 +0800

    todo: fix hw2 q4 bug

--- hw2/q4/hw2_q4.cpp
+++ hw2/q4/hw2_q4.cpp
@@ -34,6 +34,8 @@ bool operator>(const segment &s1, const segment &s2){
 	return s1.R > s2.R;
 }
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
 	long long N, M, K;
 	cin>>N>>M>>K;
 	vector<segment> segment_set;
@@ -44,42 +46,24 @@ int main(){
 		segment_set.push_back(seg_in);
 	}
 	//sort by L_i
-	sort(segment_set.begin(), segment_set.end(), sortbyL);
-	
-	#ifdef DEBUG0
-	for(long long i=0; i<N; i++)
-		cout<<segment_set[i].id<<" "<<segment_set[i].L<<" "<<segment_set[i].R<<" "<<segment_set[i].W<<endl;
-	#endif
-	
+	sort(segment_set.begin(), segment_set.end(), sortbyL);	
 	//init heap
 	priority_queue<segment, vector<segment>, less<vector<segment>::value_type>> seg_heap;
-	
-	#ifdef DEBUG0
-	for(long long i=0; i<N; i++)
-		seg_heap.push(segment_set[i]);
-	while(!seg_heap.empty()){
-		cout<<seg_heap.top().id<<" "<<seg_heap.top().L<<" "<<seg_heap.top().R<<" "<<seg_heap.top().W<<endl;
-		seg_heap.pop();
-	}
-	#endif
-	
 	long long L=1, R=K;
 	long long target=0;
-	#ifdef DEBUG
-	cout<<"start main"<<endl;
-	#endif
 	while(L<=R){
 		long long total=0;
-		long long m=(L+R)/2;
+		long long m=L+(R-L)/2;
 		#ifdef DEBUG
 		cout<<"mid= "<<m<<endl;
 		#endif
 		long long sum=0;
-		long long expired_seg_record[M+1]={0};
+		long long expired_seg_record[M]={0};//to record when should select segment from 0~M-1
 		long long j=0;
 		bool flag=0;
 		while(!seg_heap.empty())
 			seg_heap.pop();
+		//don't need to consider cell M
 		for(long long i=0; i<M; i++){
 			while(j<segment_set.size()&&segment_set[j].L==i){
 				seg_heap.push(segment_set[j]);
@@ -99,39 +83,38 @@ int main(){
 					else if(seg_heap.top().W>(m-sum)){
 						total+=(m-sum);
 						expired_seg_record[seg_heap.top().R]+=(m-sum);
-						seg_heap.push(segment(seg_heap.top().id, seg_heap.top().L, seg_heap.top().R, seg_heap.top().W-(m-sum)));
+						segment seg_temp(seg_heap.top().id, seg_heap.top().L, seg_heap.top().R, seg_heap.top().W-(m-sum));
 						seg_heap.pop();
-						if(seg_heap.top().W==0)
-							seg_heap.pop();
+						seg_heap.push(seg_temp);
 						sum+=(m-sum);
 					}
 				}
 				else{
-					flag=1;
+					flag=1;//couldn't select enough segments
 					break;
 				}
 			}
 		}
+		//if couldn't select enough segments, then search left problem
 		if(flag==1){
 			R=m-1;
 			continue;
 		}
+		//could select enough seg and meet the goal we set initially, search right problem
 		else if(total<=K){
 			target=m;
 			L=m+1;
 		}
+		//could meet the goal we set but the total segment we choose exceed K
 		else if(total>K)
 			R=m-1;
 		#ifdef DEBUG
 		cout<<"total= "<<total<<" target= "<<target<<endl;
 		#endif
 	}
-	#ifdef DEBUG
-	cout<<"end main"<<endl;
-	#endif
 	long long total=0;
 	long long sum=0;
-	long long expired_seg_record[M+1]={0};
+	long long expired_seg_record[M]={0};
 	long long j=0;
 	while(!seg_heap.empty())
 		seg_heap.pop();
@@ -157,7 +140,7 @@ int main(){
 				segment_set[seg_heap.top().id].selected_num+=(target-sum);
 				seg_heap.push(segment(seg_heap.top().id, seg_heap.top().L, seg_heap.top().R, seg_heap.top().W-(target-sum)));
 				seg_heap.pop();
-				if(seg_heap.top().W==0)
+				if(seg_heap.top().W<=0)
 					seg_heap.pop();
 				sum+=(target-sum);
 			}
