ec92da85f0887c7c8bcb25eb2c948d8c470fd5a2
mistel1225 <ha19971225@gmail.com>
2020-12-07 21:07:35 +0800

    123

--- hw3/q1/hw3_q1_v0.cpp
+++ hw3/q1/hw3_q1_v0.cpp
@@ -7,11 +7,9 @@ class vertex{
 	public:
 		long long node;
 		long long weight;
-		long long sccCnt;
 		vertex(long long node, long long weight){
 			this->node = node;
 			this->weight = weight;
-			this->sccCnt = 0;
 		}
 };
 
@@ -22,21 +20,15 @@ void dfs1(long long &node_i, vector<vector<vertex>> &adj_list, vector<long long>
 			dfs1(adj_list[node_i][j].node, adj_list, fin_list, vis);
 	fin_list.push_back(node_i);
 }
-long long dfs2(long long &node_i, long long &sccCnt, vector<vector<vertex>> &SCC_list, vector<vector<vertex>> &adj_list, vector<vector<vertex>> &adj_list_rev, vector<bool> &vis){
+long long dfs2(long long &node_i, long long sccCnt, vector<long long> &SCC_list, vector<vector<vertex>> &adj_list, vector<vector<vertex>> &adj_list_rev, vector<bool> &vis){
 	vis[node_i]=1;
 	long long weight=0;
-	adj_list[node_i][0].sccCnt = sccCnt;
+	SCC_list[node_i] = sccCnt;
 	for(long long j=1; j<adj_list_rev[node_i].size(); j++){
 		if(!vis[adj_list_rev[node_i][j].node])
 			weight += adj_list_rev[node_i][j].weight + dfs2(adj_list_rev[node_i][j].node, sccCnt, SCC_list, adj_list, adj_list_rev, vis);
-		else if(sccCnt == adj_list[adj_list_rev[node_i][j].node][0].sccCnt)
+		else if(sccCnt==SCC_list[adj_list_rev[node_i][j].node])
 			weight += adj_list_rev[node_i][j].weight;
-		else if(sccCnt != adj_list[adj_list_rev[node_i][j].node][0].sccCnt){
-			long long src = adj_list[adj_list_rev[node_i][j].node][0].sccCnt;
-			long long tar = sccCnt;
-			long long weight = adj_list_rev[node_i][j].weight;
-			SCC_list[src].push_back(vertex(tar, weight));
-		}
 	}
 	weight += adj_list_rev[node_i][0].weight;
 	return weight;
@@ -63,7 +55,7 @@ int main(){
 	for(long long i=0; i<N; i++)
 		for(long long  j=1; j<adj_list[i].size(); j++)
 			adj_list_rev[adj_list[i][j].node].push_back(vertex(i, adj_list[i][j].weight));
-	#ifdef DEBUG1
+	#ifdef DEBUG
 	cout<<"print adj_list:"<<endl;
 	for(long long i=0; i<N; i++){
 		cout<<i+1<<"th: "<<endl;
@@ -82,39 +74,23 @@ int main(){
 	for(long long i=0; i<N; i++)
 		vis[i]=0;
 	long long sccCnt = 0;
-	vector<vector<vertex>> SCC_list;
+	vector<long long> SCC_weight_list;
+	vector<long long> SCC_list(N);
 	for(long long i=N-1; i>=0; i--){
 		long long SCC_weight=0;
 		if(!vis[fin_list[i]]){
-			SCC_list.push_back(vector<vertex>(1, vertex(sccCnt, 0)));
 			SCC_weight = dfs2(fin_list[i], sccCnt, SCC_list, adj_list, adj_list_rev, vis);
-			SCC_list[sccCnt][0].weight = SCC_weight;
+			SCC_weight_list.push_back(SCC_weight);
 			sccCnt++;
 		}
 	}
 	#ifdef DEBUG
-	cout<<"====================="<<endl;
-    cout<<"print SCC_list:"<<endl;
-    for(long long i=0; i<sccCnt; i++){
-        cout<<i<<"th component weight: "<<SCC_list[i][0].weight<<endl;
-        for(long long j=1; j<SCC_list[i].size(); j++)
-            cout<<SCC_list[i][j].node<<" weight:"<<SCC_list[i][j].weight<<" ";
-        cout<<endl;
-    }   
-	#endif
-	
-	//bellman-ford algo
-	long long distance[sccCnt];
-	for(long long i=1; i<sccCnt; i++){
-		distance[i] = SCC_list[i][0].weight;
-	}
-	distance[0] = SCC_list[0][0].weight;
-	for(long long i=0; i<sccCnt; i++)
-		for(long long j=1; j<SCC_list[i].size(); j++)
-			if(distance[SCC_list[i][j].node]<distance[i]+SCC_list[i][j].weight+SCC_list[SCC_list[i][j].node][0].weight)
-				distance[SCC_list[i][j].node] = distance[i]+SCC_list[i][j].weight+SCC_list[SCC_list[i][j].node][0].weight;
-	cout<<"========================"<<endl;
-	for(long long i=0; i<sccCnt; i++)
-		cout<<distance[i]<<" ";
+	cout<<"================"<<endl;
+	cout<<"print SCC info"<<endl;
+	for(long long i=0; i<SCC_list.size(); i++)
+		cout<<i+1<<" node is belong to "<<SCC_list[i]<<" component"<<endl;
 	cout<<endl;
+	for(long long i=0; i<SCC_weight_list.size(); i++)
+		cout<<i<<" component's weight is "<<SCC_weight_list[i]<<endl;
+	#endif
 }
