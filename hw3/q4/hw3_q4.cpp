8d581add9bcf8ef5d3fd548f65824e7e243c6e88
mistel5277 <ha19971225@gmail.com>
2020-12-11 01:26:07 +0800

    hw3 q4 done

--- hw3/q4/hw3_q4.cpp
+++ hw3/q4/hw3_q4.cpp
@@ -10,17 +10,20 @@ class Group{
 		long long type;
 		long long peopleNum;
 		long long persuadeNum;
+		long long timeIn;
 		Group(){
 			this->type = -1;
 			this->peopleNum = 0;
 			this->location = 0;
 			this->persuadeNum = -1;
+			this->timeIn = 0;
 		}
-		Group(long long location, long long type, long long peopleNum, long long persuadeNum){
+		Group(long long location, long long type, long long peopleNum, long long persuadeNum, long long timeIn){
 			this->type = type;
 			this->peopleNum = peopleNum;
 			this->location = location;
 			this->persuadeNum = persuadeNum;
+			this->timeIn = timeIn;
 		}
 };
 
@@ -42,70 +45,57 @@ int main(){
 			location = 0;
 		else if(loc=="back")
 			location = 1;
-		group.push_back(Group(location, type, peopleNum, persuadeNum));
-	}
-	long long totalNum[N]={0};
-	for(long long i=0; i<Q; i++){
-		totalNum[group[i].type-1] += group[i].peopleNum*(Q-i);
-	}
-	for(long long i=0; i<N; i++)
-		cout<<totalNum[i]<<" ";
-	cout<<endl;
+		group.push_back(Group(location, type, peopleNum, persuadeNum, i));
+	}	
+
+	group[0].persuadeNum = 0;
 	deque<Group> dq;
 	dq.push_back(group[0]);
 	long long totalNum[N]={0};
-	totalNum[dq[0].type-1]+=dq[0].peopleNum;
+
 	for(long long i=1; i<Q; i++){
+		group[i].peopleNum+=group[i].persuadeNum;
+		//front
 		if(group[i].location==0){
-			for(long long j=0; j<dq.size(); j++){
-				if(group[i].persuadeNum==0){
-					break;
+			while(group[i].persuadeNum!=0){
+				long long j = 0;
+				if(group[i].persuadeNum<dq[j].peopleNum){
+					totalNum[dq[j].type-1] += dq[j].peopleNum * (i-dq[j].timeIn);
+					dq[j].peopleNum -= group[i].persuadeNum;
+					dq[j].timeIn = i;
+					group[i].persuadeNum = 0;
 				}
-				if(group[i].type!=dq[j].type && group[i].persuadeNum<=dq[j].peopleNum){
-					dq[j].peopleNum-=group[i].persuadeNum;
-					totalNum[dq[j].type-1] += dq[j].peopleNum;
-					group[i].peopleNum+=group[i].persuadeNum;
-					group[i].persuadeNum=0;
-				}
-				else if(group[i].type!=dq[j].type && group[i].persuadeNum>dq[j].peopleNum){
-					group[i].peopleNum+=dq[j].peopleNum;
+				else if(group[i].persuadeNum>=dq[j].peopleNum){
 					group[i].persuadeNum-=dq[j].peopleNum;
-					dq[j].peopleNum=0;
-				}
-				if(group[i].type==dq[j].type){
-					group[i].peopleNum += dq[j].peopleNum;
-					dq[j].peopleNum=0;
+					totalNum[dq[j].type-1] += dq[j].peopleNum * (i-dq[j].timeIn);
+					dq.pop_front();
 				}
 			}
 			dq.push_front(group[i]);
 		}
+		//back
 		else if(group[i].location==1){
-			for(long long j=dq.size()-1; j>=0; j--){
-				if(group[i].persuadeNum==0){
-					break;
+			while(group[i].persuadeNum!=0){
+				long long j = dq.size()-1;
+				if(group[i].persuadeNum<dq[j].peopleNum){
+                    totalNum[dq[j].type-1] += dq[j].peopleNum * (i-dq[j].timeIn);
+                    dq[j].peopleNum -= group[i].persuadeNum;
+                    dq[j].timeIn = i;
+                    group[i].persuadeNum = 0;
 				}
-				if(group[i].type!=dq[j].type && group[i].persuadeNum<=dq[j].peopleNum){
-					dq[j].peopleNum-=group[i].persuadeNum;
-					group[i].peopleNum+=group[i].persuadeNum;
-					group[i].persuadeNum=0;
-				}
-				else if(group[i].type!=dq[j].type && group[i].persuadeNum>dq[j].peopleNum){
-					group[i].peopleNum += dq[j].peopleNum;
-					group[i].persuadeNum -= dq[j].peopleNum;
-					dq[j].peopleNum=0;
-				}
-				if(group[i].type==dq[j].type){
-					group[i].peopleNum += dq[j].peopleNum;
-					dq[j].peopleNum=0;
+				else if(group[i].persuadeNum>=dq[j].peopleNum){
+                    group[i].persuadeNum-=dq[j].peopleNum;
+                    totalNum[dq[j].type-1] += dq[j].peopleNum * (i-dq[j].timeIn);
+                    dq.pop_back();
 				}
 			}
 			dq.push_back(group[i]);
 		}
-		for(long long i=0; i<dq.size(); i++){
-			totalNum[dq[i].type-1] += dq[i].peopleNum;
-		}
 	}
+	for(long long i=0; i<dq.size(); i++)
+		totalNum[dq[i].type-1] += dq[i].peopleNum*(Q-dq[i].timeIn);
 	for(long long i=0; i<N; i++)
 		cout<<totalNum[i]<<" ";
 	cout<<endl;
+	return 0;
 }
